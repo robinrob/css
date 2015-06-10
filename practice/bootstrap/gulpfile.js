87e0029b543befe175e251dc408f8d4ea7efbb10
Robin Smith <robin@mrrobinsmith.com>
2015-06-10 17:58:40 +0100

    Hamilise

---
+++ practice/bootstrap/gulpfile.js
@@ -0,0 +1,57 @@
+var gulp        = require('gulp');
+var browsersync = require('browser-sync');
+var reload      = browsersync.reload
+var watch       = require('gulp-watch');
+var haml        = require('gulp-ruby-haml');
+var changed     = require('gulp-changed');
+var runsequence = require('run-sequence')
+var gcallback = require('gulp-callback')
+var plumber = require('gulp-plumber')
+require('shelljs/global')
+
+var messages = {
+  hamlBuild: '<span style="color: grey">Running:</span> $ haml'
+};
+
+function onError(err) {
+  console.log(err)
+  exec('say what the fuck')
+}
+
+gulp.task('reload', function () {
+  browsersync.reload()
+});
+
+
+gulp.task('browser-sync', function() {
+  browsersync({
+    server: {
+      baseDir: '.'
+    },
+    browser: "safari"
+  });
+});
+
+// Watch for changes in Haml files
+gulp.task('haml-watch', function() {
+  var dest = '.'
+  gulp.src('./*.haml').
+    pipe(plumber({
+      onError: onError
+    })).
+    pipe(watch('./*.haml')).
+    pipe(changed(dest, {extension: '.html'})).
+    pipe(haml()).
+    pipe(gulp.dest(dest)).
+    pipe(gcallback(reload))
+})
+
+// gulp.task('reload', function() {
+//   reload()
+// })
+
+gulp.task('watch', function () {
+  gulp.watch('*.js', reload)
+});
+
+gulp.task('default', ['browser-sync', 'watch', 'haml-watch']);
